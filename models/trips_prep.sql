7b644d77fedfd04953eb63cf223359898e5703eb
Pavel Sharapov <pavsharapov+1@gmail.com>
2024-08-18 07:13:47 +0000

    a

--- models/trips_prep.sql
+++ models/trips_prep.sql
@@ -10,6 +10,6 @@ finish_lon,
 distance as distance_m, 
 cast(price as decimal(20,2))/100 as price_rub,
 extract(epoch from (finished_at - started_at)) as duration_s,
-date(started_at) as "date",
+    {{ date_in_moscow('started_at') }} as "date",
  finished_at <> started_at and price = 0 as is_free
 FROM {{ source("scooters_raw", "trips") }}
